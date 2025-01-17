771bc2501881c410962072a8e90ac37fcd57bce0
jerrysanguyo <jsanguyo1624@gmail.com>
2025-01-17 19:43:01 +0800

    done with Layout and sample dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,61 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Include AdminLTE CSS -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+    <!-- DataTable -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-
-    <style>
-        .main-header{
-            margin-left: 0px !important;
-        }
-    </style>
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/4f2d7302b1.js" crossorigin="anonymous"></script>
+    <!-- External CSS -->
+    <link rel="stylesheet" href="{{ asset('css/appCss.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
-        @yield('content')
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- upper navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                        <i class="fas fa-bars"></i>
+                    </a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-white">Home</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-white">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- Side bar -->
+        @include('layouts.sidebar.appSidebar')
+        <div class="content-wrapper content-bg">
+        <!-- Main Content -->
+        <div class="col-lg-12">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Sample</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Sample</li>
+                            </ol>
+                        </div>
+                    </div>
+                </header>
+            </div>
+            <!-- Page Content -->
+            <main>
+                @yield('content')
+            </main>
+        </div>
+        <footer class="main-footer text-white fixed-bottom py-2 position-fixed bottom-0 p-3">
+            <strong>Copyright © 2025 Information technology - City of Taguig.</strong> All right reserved.
+        </footer>
     </div>
-
     <!-- Include jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Include Bootstrap -->
---
+++ resources/views/layouts/sidebar/appSidebar.blade.php
@@ -0,0 +1,98 @@
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <a href="index3.html" class="brand-link">
+        <img src="{{ asset('images/CL.webp') }}" alt="City logo" class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light text-white">Assessor</span>
+    </a>
+
+    <div class="sidebar">
+        <div class="user-panel d-flex mb-3 mt-3 pb-3">
+            <div class="image">
+                <img src="{{ asset('images/default.webp') }}" class="img-circle elevation-2" alt="User picture">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Jerf Leo Carpio</a>
+            </div>
+        </div>
+
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-search fa-fw"></i>
+                        <p>Search</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-chart-line"></i>
+                        <p>Transaction Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-key"></i>
+                        <p>PIN</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#trackingModal">
+                        <i class="nav-icon fas fa-map-marker-alt"></i>
+                        <p>Tracking</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#reportModal">
+                        <i class="nav-icon fas fa-file-alt"></i>
+                        <p>Report</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#printingModal">
+                        <i class="nav-icon fas fa-print"></i>
+                        <p>Printing</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-check-circle"></i>
+                        <p>Validation</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#messageModal">
+                        <i class="nav-icon fas fa-envelope"></i>
+                        <p>Messages</p>
+                    </a>
+                </li>
+            </ul>
+            <hr class="mt-4 mb-3">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-header">Administrator Menu</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#adminModal">
+                        <i class="nav-icon fas fa-user-shield"></i>
+                        <p>Administrator Menu</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#accountInfo">
+                        <i class="nav-icon fas fa-user-edit"></i>
+                        <p>Edit Account Info</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-key"></i>
+                        <p>Change Password</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
